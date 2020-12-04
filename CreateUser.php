e7eadc40521320d439ac4e76ddd2049dfd7e9ce5
Trey Werr <treywerr@gmail.com>
2020-12-04 10:42:48 -0600

    Testing database connection

--- CreateUser.php
+++ CreateUser.php
@@ -1,5 +1,5 @@
 <?php
-/**
+
 $mysqli = new mysqli("mysql.eecs.ku.edu", "treywerr", "AiNg4gue",
 "treywerr");
 /* check connection */
@@ -7,7 +7,7 @@ if ($mysqli->connect_errno) {
  printf("Connect failed: %s\n", $mysqli->connect_error);
  exit();
 }
-**/
+
 $user = $_POST["username"];
 
 if ($user == "") {
@@ -15,18 +15,6 @@ if ($user == "") {
 } else {
 	echo "<h3>Successfully created new user " . $user . "!</h3>";
 }
-
-/**
-$query = "SELECT Name, CountryCode FROM City ORDER by ID DESC LIMIT 50,5";
-if ($result = $mysqli->query($query)) {
- /* fetch associative array */
- while ($row = $result->fetch_assoc()) {
- printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
- }
- /* free result set */
- $result->free();
-}
-**/
 
 /* close connection */
 $mysqli->close();
